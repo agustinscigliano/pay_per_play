ace87fbc8b55947f96d6b66a8271151c0f8adfad
Agustin Scigliano <aguscigliano@gmail.com>
2017-11-26 20:29:34 -0300

    Version without burnable token

--- contracts/PayPerPlayToken.sol
+++ contracts/PayPerPlayToken.sol
@@ -1,9 +1,8 @@
 pragma solidity ^0.4.18;
 import 'zeppelin-solidity/contracts/token/MintableToken.sol';
-import 'zeppelin-solidity/contracts/token/BurnableToken.sol';
 import 'zeppelin-solidity/contracts/token/PausableToken.sol';
 
-contract PayPerPlayToken is MintableToken, BurnableToken, PausableToken {
+contract PayPerPlayToken is MintableToken, PausableToken {
 string public name = "PayPerPlay"; 
 string public symbol = "PPP";
 uint public decimals = 18;
