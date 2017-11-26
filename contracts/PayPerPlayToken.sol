1618a5ff33cf55adeeb0be11265b15eabe5ae483
Jose Noriega <jose.noriega.defferrari@gmail.com>
2017-11-26 17:14:46 -0300

    PPP is now pausable

--- contracts/PayPerPlayToken.sol
+++ contracts/PayPerPlayToken.sol
@@ -1,10 +1,12 @@
 pragma solidity ^0.4.18;
 import '/zeppelin-solidity/contracts/token/MintableToken.sol';
 import '/zeppelin-solidity/contracts/token/BurnableToken.sol';
+import '/zeppelin-solidity/contracts/token/PausableToken.sol';
 
-contract PayPerPlay is MintableToken, Burnable{
+contract PayPerPlayToken is MintableToken, BurnableToken, PausableToken {
 string public name = "PayPerPlay"; 
 string public symbol = "PPP";
 uint public decimals = 18;
 
 }
+
