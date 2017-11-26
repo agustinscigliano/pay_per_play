ac949e9aaa90b3d6edea66c4ba5c007dc038a533
Agustin Scigliano <aguscigliano@gmail.com>
2017-11-26 20:08:18 -0300

    Changes for deployment

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,18 +1,13 @@
-/*const TGCCrowdsale = artifacts.require("./PayPerPlayICO.sol")
+const PayPerPlayICO = artifacts.require("./PayPerPlayICO.sol")
 
 module.exports = function(deployer, network, accounts) {
  //TODO: Remove this when deploy.
- const startTime = web3.eth.getBlock(web3.eth.blockNumber).timestamp + 1
- const endTime = startTime + (86400 * 20) 
- // const startTime = 1511730000; // 		6:00:00 PM GMT-03:00 - 26/11/17
- // const endTime = 1511920800; // 			11:00:00 PM GMT-03:00 - 28/11/17
+ //const startTime = web3.eth.getBlock(web3.eth.blockNumber).timestamp + 1
+ //const endTime = startTime + (86400 * 20) 
+ const startTime = 1511739000; // 		20:30:00 GMT-03:00 - 26/11/17
+ const endTime = 1511920800; // 			11:00:00 PM GMT-03:00 - 28/11/17
  const rate = new web3.BigNumber(1000)
  const wallet = accounts[0]
 
  deployer.deploy(PayPerPlayICO, startTime, endTime, rate, wallet)
-};*/
-var PayPerPlay = artifacts.require("./PayPerPlay.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(PayPerPlay);
 };
